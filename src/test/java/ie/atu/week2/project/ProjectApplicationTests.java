6057874ac6901cbc1f44f5765a47b26b34b22a9d
seankeogh1 <G00432475@atu.ie>
2025-09-30 14:21:46 +0100

    initial commit

---
+++ src/test/java/ie/atu/week2/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
