b3c93d6038e55f07052d4b6f6f5d631b5ee1e8d6
adamo <g00434897@atu.ie>
2025-10-07 15:55:59 +0100

    Started adding post mapping and get requests in RequestController class

--- src/main/java/ie/atu/week2/project/RequestController.java
+++ src/main/java/ie/atu/week2/project/RequestController.java
@@ -1,10 +1,36 @@
 package ie.atu.week2.project;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/UserApp")
 @RestController
-@RequestMapping
 
 public class RequestController {
+
+    List<User> myUsers = new ArrayList<User>();
+
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to UserApp!";
+    }
+
+    @GetMapping("/User Info")
+    public User getUser() {
+        User myUser = new User("subhan","john","adamok@gamil.com","trex","america");
+        return myUser;
+    }
+
+    @PostMapping("/addUser")
+    public User addUser(@Valid @RequestBody User myUser) {
+        myUsers.add(myUser);
+        return myUser;
+    }
+
 
 
 }
