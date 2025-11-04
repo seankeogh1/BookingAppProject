287a8aeaa9a4280655de79dd15860342442249be
ADAMOKEEFFE-STUDENT <g00434897@atu.ie>
2025-11-04 19:25:16 +0000

    Added delete in controller and added a hash memory to be able to delete a crd by its number

--- src/main/java/ie/atu/week2/project/Payment/PaymentController.java
+++ src/main/java/ie/atu/week2/project/Payment/PaymentController.java
@@ -2,17 +2,31 @@ package ie.atu.week2.project.Payment;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
 
+    //card storage keeps the card number
+    private Map<String, Payment> payments = new HashMap<>();
+
     @PostMapping
     public ResponseEntity<String> processPayment(@Valid @RequestBody Payment payment) {
         return ResponseEntity.ok("Payment processed successfully for " + payment.getNameOnCard());
     }
+
+    @DeleteMapping("/{cardNumber}")
+    public ResponseEntity<String> deleteCardDetails(@PathVariable String cardNumber){
+        if(payments.containsKey(cardNumber)){
+            payments.remove(cardNumber);
+            return ResponseEntity.ok("Card details deleted successfully.");
+        }else{
+            return ResponseEntity.status(404).body("Card not found");
+        }
+    }
+
 }
